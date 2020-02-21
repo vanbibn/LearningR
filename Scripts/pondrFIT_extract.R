1bc6934f7be9e39d200471586ab0fe1568b0e5bb
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-02-20 23:26:10 -0500

    Reworked data extraction. Started creating a function to calculate mean and percent disorder scores for each data file in a folder.

--- Scripts/pondrFIT_extract.R
+++ Scripts/pondrFIT_extract.R
@@ -3,15 +3,44 @@ library(readr)
 library(stringr)
 
 # paste in temporary url fro PONDR-FIT data
-u <- url("http://original.disprot.org/temp/1582165344.182021.pondrfit")
-fit <- readLines(u)
+u <- url("http://original.disprot.org/temp/1582230290.859100.pondrfit")
+
+# Create a data frame start reading from line 2 (skip first line)
+fit1 <- read_table2(u, col_names = FALSE, skip = 1)
 close(u)
 
-#remove header row and extra spaces between and before "columns"
-fit1 <- fit[2:length(fit)]
-fit1 <- gsub("\\s+", " ", str_trim(fit1))
+# add column names and write out to text file
+colnames(fit1) <- c("position", "residue", "score", "error")
+write.csv(fit1, file = "Output/test_pondrFIT.csv")
 
+# new function to calculate mean and % disorder scores
+pfit <- function(directory, ...) {
+    # create an empty data frame with 3 named columns 
+    fit_df <- data.frame(matrix(ncol = 3, nrow = 0))
+    colnames(fit_df) <- c("uniprotID","meanDisorder","percentDisorder")
+    
+    #begin for loop here
+    for (file in folder) {
+        # open csv file
+        df <- read.csv("Output/test_pondrFIT.csv")
+        
+        # extract uniprot ID form file name and save to a variable
+        uniprotID <- "test"
+        
+        #calculate the mean disorder score
+        meanDisorder <- mean(df$score)
+        
+        # Percent disorder: count num resid score >0.5 and divid by total num residues
+        total <- length(df$score)
+        numDis <- sum(df$score > 0.5)
+        percDisorder <- numDis/total
+        
+        # concatinate these values to growing matrix
+        fit_df <- rbind(fit_df,data.frame(uniprotID, meanDisorder, percDisorder))
+    }
+    
+    round(fit_df$meanDisorder, digits = 3)
+    round(fit_df$percentDisorder, digits = 3)
+    fit_df
+}
 
-df1 <- strsplit(fit1, " ") # makes a list not a df
-df2 <- as.data.frame(str_split(fit1, " ")) #makes a df but wrong orientation
-m1 <- as.matrix(str_split(fit1, " ")) #str_split must still make it a list
