4f17c39541bdf0b9b09460366fb5a30c9f68e72f
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-05-06 22:53:45 -0400

    List of packages to install after a new version update.

---
+++ Scripts/Rv4_reInstallPackages.R
@@ -0,0 +1,11 @@
+# list of packages to reinstall after updating to R version 4.0.0 (2020-04-24) -- "Arbor Day"
+
+
+install.packages("tidyverse")
+install.packages("xaringan")
+install.packages("XML")
+install.packages("swirl")
+install.packages("RSelenium")
+install.packages("roxygen2")
+install.packages("testthat")
+install.packages("readxl")
